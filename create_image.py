a26aaca0affd747cb9f5478dbc124ab871e2a911
Brandon Wilde <brandon.t.wilde@gmail.com>
2024-06-27 10:20:59 -0700

    Specify image generation model, print revised prompt

--- create_image.py
+++ create_image.py
@@ -4,16 +4,19 @@ from media_tools.image_tools import generate_image_via_openai
 from utils import increment_file_name
 
 
-def create_image(prompt, output_file, size="1024x1024"):
+def create_image(prompt, output_file, model="dall-e-3", size="1024x1024"):
     '''
     Create an image using the OpenAI API.
     '''
 
     # Create image
-    image_response = generate_image_via_openai(prompt, size=size)
+    image_response = generate_image_via_openai(prompt, model=model, size=size)
 
     for image in image_response.data:
-        print(image.revised_prompt)
+        if hasattr(image, 'revised_prompt'):
+            print('Revised prompt:', image.revised_prompt)
+        else:
+            print('Original prompt used.')
 
         # download image
         image = requests.get(image.url)
@@ -27,5 +30,7 @@ def create_image(prompt, output_file, size="1024x1024"):
 
 output_file = "./transcription_splash.png"
 prompt = "A splash of color with the word 'transcription' in the center."
+size = "512x512"
+model = "dall-e-2"
 
-create_image(prompt, output_file)
+create_image(prompt, output_file, model=model, size=size)
