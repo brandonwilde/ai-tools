641075022a405403cb3a1f98dce295733b770f28
Brandon Wilde <brandon.t.wilde@gmail.com>
2024-07-02 22:58:42 -0700

    Add chat template

---
+++ templates/chat.py
@@ -0,0 +1,8 @@
+from media_tools.text_tools import chat_with_openai
+
+
+messages = [
+    {"text": "When was the first time you heard about the concept of a chatbot?"}
+]
+
+chat_with_openai(messages)
