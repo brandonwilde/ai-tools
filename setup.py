845cb099279ae5a2facbfbf872b02d8fb6bfdd9b
Brandon Wilde <brandon.t.wilde@gmail.com>
2024-10-09 21:10:04 -0500

    Make installation use latest reqs

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from setuptools import setup, find_packages
 
+def read_requirements():
+    """Read the requirements.txt file and return a list of dependencies."""
+    with open('requirements.txt') as f:
+        return f.read().splitlines()
+
 setup(
     name='aitools',
     version='0.1',
     packages=find_packages(),
-    install_requires=[
-        "openai==1.30.4",
-        "tabulate==0.9.0",
-    ],
+    install_requires=read_requirements(),
     author='Brandon T Wilde',
     author_email='brandon.t.wilde@gmail.com',
     description='A collection of general-purpose tools for processing text, documents, images, and videos.',
